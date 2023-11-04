d7b621061433b5f28a2a4197559803836f38fa5c
Mamadou <senemamadou1999@gmail.com>
2023-11-04 14:11:35 +0000

    ajout de la fonction pour requeter les structures de santé

--- lib/providers/ProviderCovid.dart
+++ lib/providers/ProviderCovid.dart
@@ -1,9 +1,14 @@
+import 'dart:convert';
+
+import 'package:client_covid/models/StructureDeSante.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ProviderCovid extends ChangeNotifier {
   Icon icon = Icon(Icons.visibility);
   bool visibility = false;
+  static List<StructureDeSante> listStructures = [];
   void change_Visibility() {
     visibility = !visibility;
     notifyListeners();
@@ -17,5 +22,21 @@ class ProviderCovid extends ChangeNotifier {
       icon = Icon(Icons.visibility);
       notifyListeners();
     }
+  }
+
+  Future<List<StructureDeSante>> getStructures() async {
+    String url = "http://localhost:8080/REST_COVID_WEB_SERVICE/rs/structures";
+    http.Response response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      print("cible atteint");
+    } else {
+      print("error connection");
+    }
+    var responseJson = jsonDecode(response.body);
+    for (var s in responseJson[""]) {
+      listStructures.add(StructureDeSante.fromJson(s));
+    }
+    print(listStructures);
+    return listStructures;
   }
 }
