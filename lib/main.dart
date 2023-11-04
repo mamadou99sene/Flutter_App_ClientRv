014e9e41d1e74baf73ed2ee73be70b7da11d0e18
Mamadou <senemamadou1999@gmail.com>
2023-11-04 14:12:28 +0000

    suppression des imports non utilisés

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
-import 'package:client_covid/App.dart';
 import 'package:client_covid/Home.dart';
-import 'package:client_covid/pages/LoginPage.dart';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:http/http.dart';
-import 'package:provider/provider.dart';
 
 main() {
   runApp(Home());
