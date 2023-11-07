a7679d93931afdefa68516813565529d771bcfbd
Mamadou <senemamadou1999@gmail.com>
2023-11-07 15:50:57 +0000

    ajout du model utilisateur

---
+++ lib/models/Utilisateurs.dart
@@ -0,0 +1,15 @@
+class Utilisateur {
+  late int idUtilisateur;
+  late String prenom;
+  late String nom;
+  late String email;
+  late String phone;
+  late String password;
+  Utilisateur(
+      {required this.idUtilisateur,
+      required this.prenom,
+      required this.nom,
+      required this.email,
+      required this.phone,
+      required this.password});
+}
