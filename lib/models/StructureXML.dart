6e4511417aeb7abe6ae6907ffe253c82f4a31368
Mamadou <senemamadou1999@gmail.com>
2023-11-07 15:50:02 +0000

    ajout du model Structure pour les données xml

---
+++ lib/models/StructureXML.dart
@@ -0,0 +1,29 @@
+import 'package:xml/xml.dart';
+
+class StructureXML {
+  late int idStructure;
+  late String localisation;
+  late int capacite;
+  late String typeTraitement;
+  late String telephone;
+  late String email;
+  late String typeTest;
+
+  StructureXML(
+      {required this.idStructure,
+      required this.localisation,
+      required this.capacite,
+      required this.typeTraitement,
+      required this.email,
+      required this.typeTest,
+      required this.telephone});
+  StructureXML.fromXML(XmlElement xmlData) {
+    idStructure = xmlData.findElements("idStructuresante").first.text as int;
+    /*localisation = xmlData["localisation"];
+    capacite = xmlData["capacite"];
+    typeTraitement = xmlData["typeTraitement"];
+    telephone = xmlData["telephone"];
+    email = xmlData["email"];
+    typeTest = xmlData["typetest"];*/
+  }
+}
