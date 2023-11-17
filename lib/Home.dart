ecea8f9db0d11af3e209513b28e6922c21eae28f
Mamadou <senemamadou1999@gmail.com>
2023-11-17 16:11:29 +0000

    ajout du multiprovider dans le home

--- lib/Home.dart
+++ lib/Home.dart
@@ -2,25 +2,30 @@ import 'package:client_covid/App.dart';
 import 'package:client_covid/pages/Inscription.dart';
 import 'package:client_covid/pages/LoginPage.dart';
 import 'package:client_covid/pages/PriseRendezVous.dart';
+import 'package:client_covid/providers/ProviderCovid.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      //home: App(),
-      routes: {
-        "/": (context) => loginPage(),
-        "/home": (context) => App(prenom: "prenom", nom: "nom"),
-        "/inscription": (context) => Inscription(),
-        "/rendezvous": (context) => PriseRendezVous(),
-      },
-      initialRoute: "/",
-      theme: ThemeData(
-        primarySwatch: Colors.red,
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => ProviderCovid())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        //home: App(),
+        routes: {
+          "/": (context) => loginPage(),
+          "/home": (context) => App(prenom: "prenom", nom: "nom"),
+          "/inscription": (context) => Inscription(),
+          "/rendezvous": (context) => PriseRendezVous(),
+        },
+        initialRoute: "/",
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
       ),
     );
   }
